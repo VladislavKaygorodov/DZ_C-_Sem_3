ae1730010e219a86b7bb271485367fd561acac3b
Vladislav <kaygorodov.vlad@gmail.com>
2022-07-03 22:06:18 +0700

    Выполненные задания

---
+++ Zad19/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да */
+
+Console.WriteLine("Введите число: ");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+int numA = numFirst;
+int numB = 0;
+while (numA > 0) 
+{
+    numB = numB * 10 + numA % 10;
+    numA /= 10;
+}
+Console.WriteLine(numB);
+
+if (numFirst == numB)
+{
+    Console.WriteLine($"Число {numFirst} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {numFirst} не является палиндромом");
+}
---
+++ Zad21/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+/*Console.WriteLine("Введите координату Х точки А: ");
+double numXA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y точки А: ");
+double numYA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z точки А: ");
+double numZA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X точки B: ");
+double numXB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y точки B: ");
+double numYB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z точки B: ");
+double numZB = Convert.ToInt32(Console.ReadLine());*/
+
+double numXA = 3;
+double numYA = 6;
+double numZA = 8;
+double numXB = 2;
+double numYB = 1;
+double numZB = -7;
+
+double FunKv(double number)
+{
+    number *= number;
+    return number;
+}
+
+double KvOne = FunKv(numXB - numXA);
+double KvTwo = FunKv(numYB - numYA);
+double KvThree = FunKv(numZB - numZA);
+
+double RasstAB = Math.Sqrt(KvOne+KvTwo+KvThree);
+
+Console.WriteLine(Math.Round(RasstAB, 2));
